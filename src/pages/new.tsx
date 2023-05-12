60002e88a61e1546e2b53d07881a89222f2a1fbc
angus <angusmiguel21@gmail.com>
2023-05-12 19:48:32 +0100

    tauri and exe

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,24 +1,32 @@
 
 import { Database } from "@/lib/types";
-import { GetServerSidePropsContext } from "next";
 import { Navbar, NewLink } from "@/components";
 import { SupaAuth } from "@/components/supa_auth";
 import { User } from "@/state";
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-	const supa = createServerSupabaseClient<Database>(ctx);
-	const users = supa.from("profiles").select("username, id");
-	return {
-		props: {
-			users: process.env.NODE_ENV === "production" ?
-				((await users).data ?? []).filter(user => user.id !== "6550a93e-69c4-45ae-870e-c45f47586ceb") :
-				(await users).data
+function NewPage() {
+	const [users, setUsers] = useState<User[]>([]);
+	const supabaseClient = useSupabaseClient<Database>();
+
+	useEffect(() => {
+		async function getUsers() {
+			const { data, error } = await supabaseClient.from("profiles").select("username, id");
+			if (error) {
+				console.error({ usersError: error });
+
+			} else {
+				setUsers(data);
+			}
 		}
-	};
-}
+		getUsers();
 
-function NewPage({ users }: { users: User[] }) {
+		return () => {
+			setUsers([]);
+		};
+	}, [supabaseClient]);
+
 	return (
 		<SupaAuth>
 			<Navbar />
